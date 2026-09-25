3d3212407f7b2b40841280d0eb54da5cfddbe6b0
Samit2050 <116873052+Samit2050@users.noreply.github.com>
2026-09-25 12:29:31 +0530

    Expose force update status to renderer
    Added a new method to get the force update status.

--- electron/preload.js
+++ electron/preload.js
@@ -1,39 +1,27 @@
+
 import { contextBridge, ipcRenderer } from 'electron';
 
 const electronAPI = {
-  /**
-   * Get the currently installed application version.
-   */
   getAppVersion: () => {
     return ipcRenderer.invoke('get-app-version');
   },
 
-  /**
-   * Check GitHub for a newer application version.
-   */
+  getForceUpdateStatus: () => {
+    return ipcRenderer.invoke('get-force-update-status');
+  },
+
   checkForUpdates: () => {
     return ipcRenderer.invoke('check-for-updates');
   },
 
-  /**
-   * Download the available update.
-   */
   downloadUpdate: () => {
     return ipcRenderer.invoke('download-update');
   },
 
-  /**
-   * Install the downloaded update and restart the application.
-   */
   installUpdate: () => {
     return ipcRenderer.invoke('install-update');
   },
 
-  /**
-   * Listen for update status events from the Electron main process.
-   *
-   * Returns a cleanup function that removes the listener.
-   */
   onUpdateStatus: (callback) => {
     if (typeof callback !== 'function') {
       return () => {};
